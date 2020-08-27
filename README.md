a2b35ed3e27168ca57fa02545ea5d12c5bd816d2
Thaddeus Cooper <thaddeus_cooper@apple.com>
2020-08-27 15:18:29 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# dotfiles configuration installer
+
+This repo houses dotfiles for my zsh UI. Running `install.sh` will check for previous instances of the associated files, back them up (.\<file\>.bak) and symlink the files from the repo to the appropriate location.
+
+### Features
++ oh-my-zsh UI
++ Powerlevel 10k theme
++ Tmux at launch
++ Custom stat bar
+    + Music currently playing in the Apple Music app (using .applemusic script)
+    + Battery percentage (using battery.sh)
+    + Date/time
+    + Hostname
+
+** Coming Soon **
++ [ ] IP address in stat bar
++ [ ] Performance stats (CPU/Memory)
++ [ ] Configurable stock ticker
++ [ ] Weather
+
+
+### Screenshots
+
+![VIM UI](https://github.com/suckerface/dotfiles/blob/master/images/vim.png)
+
+![TMUX UI](https://github.com/suckerface/dotfiles/blob/master/images/ui.png)
---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/zsh
+
+# This script will symlink dotfiles from the git repo directory to their appropriate locations in the user's home directory
+
+# Variable declarations
+# sets CWD variable to current directory
+CWD=$(pwd)
+P10K=~/.p10k.zsh
+TMUX=~/.tmux.conf
+VIM=~/.vimrc
+ZSH=~/.zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# The next line is needed to automatically pull submodules included in the repo
+git submodule update --init --recursive
+
+if test -f "$P10K"; then
+        echo "$P10k exists, creating backup"
+        cp ~/.p10k.zsh ~/.p10k.zsh.bak
+fi
+
+if test -f "$TMUX"; then
+        echo "$TMUX exists, creating backup"
+        cp ~/.tmux.conf ~/.tmux.conf.bak
+fi
+
+if test -f "$VIM"; then
+        echo "$VIM exists, creating backup"
+        cp ~/.vimrc ~/.vimrc.bak
+fi
+
+if test -f "$ZSH"; then
+        echo "$ZSH exists, creating backup"
+        cp ~/.zsh ~/.zsh.bak
+fi
+
+# Symlinks files from repo to home directories
+ln -is $CWD/.applemusic $HOME
+ln -is $CWD/.battery.sh $HOME
+ln -is $CWD/.p10k.zsh $HOME
+ln -is $CWD/.tmux.conf $HOME
+ln -is $CWD/.vimrc $HOME
+ln -is $CWD/.zshrc $HOME
+
+# Copy/configure p10k
+ln -s $CWD/powerlevel10k $HOME/.oh-my-zsh/custom/themes/
+
+# Copy/configure vundle - has to be symlink for plugin install
+ln -s $CWD/bundle $HOME/.vim/
+
+# Install and configure Vundle options
+vim ~/.vimrc +PluginInstall +x +q!
